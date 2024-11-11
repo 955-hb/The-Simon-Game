3f337130b8016711fe5db015868fe4b7cc9a7b32
Hannes Bröhl <hannes.broehl@icloud.com>
2024-11-11 14:02:01 +0100

    --> prettier

--- game.js
+++ game.js
@@ -6,21 +6,16 @@ let buttonCollours = ["red", "blue", "green", "yellow"];
 $(".btn").click(function () {
   nextSequence();
   let userChosenColour = $(this).attr("id");
-  //console.log(userChosenColour)
 
   userClickedPattern.push(userChosenColour);
-  //console.log(userClickedPattern);
 
-  // 5.1
   playSound(userChosenColour);
 });
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-  //console.log(randomNumber)
 
   let randomChosenColour = buttonCollours[randomNumber];
-  //console.log(randomChosenColour);
 
   gamePattern.push(randomChosenColour);
 
@@ -31,10 +26,9 @@ function nextSequence() {
 
   playSond(randomChosenColour);
 }
-// 5.2
-function playSond(name){
+
+function playSond(name) {
   // 5.3
   let audio = new Audio("/sounds/" + name + ".mp3");
   audio.play();
 }
-
