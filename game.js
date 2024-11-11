af7d05e5787622a288439f9061906f8478655f57
Hannes Bröhl <hannes.broehl@icloud.com>
2024-11-11 13:30:40 +0100

    wich button is pressed!

--- game.js
+++ game.js
@@ -1,35 +1,37 @@
-//alert('Hello, it works!')
-
-// #2.5
+// 4.3
+let userClickedPattern = [];
 let gamePattern = [];
 
-// #2.3
 let buttonCollours = ["red", "blue", "green", "yellow"];
+ // 4.1
+$('.btn').click(function(){
+  // 4.2
+  let userChosenColour = $(this).attr('id');
+  //console.log(userChosenColour)
+  // 4.4
+  userClickedPattern.push(userChosenColour);
+  //console.log(userClickedPattern);
+})
 
-// #2.1
 function nextSequence() {
-  // #2.2
   let randomNumber = Math.floor(Math.random() * 4);
   //console.log(randomNumber)
 
-  // #2.4
   let randomChosenColour = buttonCollours[randomNumber];
   //console.log(randomChosenColour);
 
-  // #2.6
   gamePattern.push(randomChosenColour);
 
-  // 3.1 & 3.2
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 
-  // 3.3
   let audio = new Audio("/sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
 
 // lösung mittels Event-Listener
-$(document).on('click', function(){
+$(document).on("click", function () {
   nextSequence();
-})
-
-
+});
